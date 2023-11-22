d25732b1acd4757a1626965dac3d8ba10c3e72d7
Fabriciocod <fva.mcz@gmail.com>
2023-11-22 16:08:43 -0300

    Adicionado imagem e titulo da home

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,24 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
+function Titulo({x, y}){
+  return(
+    <h1 style={{left: x, top: y}} className={styles.titulo}>
+      More economy, same speed
+    </h1>
+  );
+}
 export default function Home() {
   return (
-    <h1></h1>
-  )
+    <>
+    <Image
+    src="/background.png"
+    width={1920}
+    height={1020}
+    alt='Fundo da Home'/>
+    
+    <Titulo></Titulo>
+    </>
+    
+  );
 }
